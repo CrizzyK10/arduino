386ed818eb04076a8c9fc93a032f77636def1c89
CrizzyK10 <34009826+CrizzyK10@users.noreply.github.com>
2018-03-05 04:27:18 +0000

    Create TembooAccount.h

---
+++ TembooAccount.h
@@ -0,0 +1,3 @@
+#define TEMBOO_ACCOUNT "crizzyk10"  // your Temboo account name 
+#define TEMBOO_APP_KEY_NAME "myFirstApp"  // your Temboo app key name
+#define TEMBOO_APP_KEY  "5vW4TLBFQHJA4aN1Wni9zgPorxFi7iLU"  // your Temboo app key
